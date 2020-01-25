3f3a7a7e8595dcaa4b2422d7ad8be6e620b6d48e
farfalle211 <farfalle211@gmail.com>
2020-01-25 16:27:12 -0600

    finished players controller, added views for players

--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -1,21 +1,45 @@
 class Api::PlayersController < ApplicationController
   def index
-    
+    @players = Player.all
+    render 'index.json.jbuilder'
   end
 
   def create
-    
+    @player = Player.new(
+                          name: params[:name],
+                          position: params[:position],
+                          team_id: params[:team_id]
+                        )
+
+    if @player.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @players.errors.full_messages}, status: :unprocessable_entity 
+    end
   end
 
   def show
-    
+    @player = Player.where(id: params[:id])
+    render 'show.json.jbuilder'
   end
 
   def update
+    @player = Player.find(params[:id])
     
+    @player.name = params[:name] || @player.name
+    @player.position = params[:position] || @player.position
+    @player.team_id = params[:team_id] || @player.team_id
+
+    if @player.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @player.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    
+    @player = Player.find(params[:id])
+    @player.destroy
+    render json: {message: "Successfully removed player"}
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,8 @@
 class Player < ApplicationRecord
+  validates :name, presence: true
+
   belongs_to :team
   has_many :updates
 end
+
+#use .valid?
